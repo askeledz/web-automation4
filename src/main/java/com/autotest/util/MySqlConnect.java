5a24bb85954dd43fdb4f86977dabdf757d8d8297
askeledz <andrej.skeledzija@outlook.com>
2018-04-02 14:33:01 +0200

    Add MySQL db test

---
+++ src/main/java/com/autotest/util/MySqlConnect.java
@@ -0,0 +1,70 @@
+package com.autotest.util;
+
+import java.sql.*;
+import java.util.Properties;
+
+
+// Sequel Pro - for Database
+// Driver Download - https://dev.mysql.com/downloads/connector/odbc/
+public class MySqlConnect {
+	// Connection object
+	private static Connection conn = null;
+	// Statement object
+	public static Statement stmt;
+	// Result Set
+	public static ResultSet results = null;
+	// Constant for Database URL
+	private static String DB_URL = "jdbc:mysql://localhost:3306/mydb"; // Oracle "jdbc:oracle:thin:@localhost:1521/sid"
+
+	// Constant for Database Username
+	private static String DB_USER = "root";
+	// Constant for Database Password
+	private static String DB_PASSWORD = "Sifra123456789";
+	// Driver
+	private static String driver = "com.mysql.jdbc.Driver"; // "oracle.jdbc.driver.OracleDriver"
+	
+	// WebDriver
+	// public static WebDriver dv;
+
+	public Connection openDB() {
+		// Intialize WebDriver
+		// dv = new FirefoxDriver();
+		
+		// Properties for creating connection to database
+		Properties props = new Properties();
+		props.setProperty("user", "root");
+		props.setProperty("password", "Sifra123456789");
+	    
+		try {
+			// STEP 1: Register JDBC driver
+			Class.forName(driver).newInstance();
+			
+			// STEP 2: Get connection to DB
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			// conn = DriverManager.getConnection(DB_URL, props);		
+			System.out.println("Connected database successfully...");
+			
+			// STEP 3: Statement object to send the SQL statement to the Database
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public void closeDb() {
+		try {
+			if (results != null)
+				results.close();
+			if (stmt != null)
+				conn.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/com/autotest/orange/admin/MySQLTest.java
@@ -0,0 +1,108 @@
+package com.autotest.orange.admin;
+
+import com.autotest.sales.pages.AdminPage;
+import com.autotest.sales.pages.LandingPage;
+import com.autotest.test.BaseTest;
+import com.autotest.util.Config;
+import com.autotest.util.MySeleniumMethods;
+import com.autotest.util.MySqlConnect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MySQLTest extends BaseTest {
+
+    private static final Logger logger = LogManager.getLogger(MySQLTest.class);
+    private static WebDriver driver = null;
+    private static ResultSet results = null;
+
+    Connection conn;
+
+    LandingPage landingPage;
+    AdminPage adminPage;
+
+    @BeforeTest
+    public void doBeforeTest() {
+
+    }
+    @AfterClass
+    public void afterClass() {
+        try {
+            if (results != null)
+                results.close();
+            System.out.println("results.close();");
+            if (MySqlConnect.stmt != null)
+                conn.close();
+            System.out.println("stmt - conn.close();");
+            if (conn != null)
+                conn.close();
+            System.out.println("conn.close();");
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
+
+    @Parameters
+    @Test(description = "MySQL Example", groups = {"checkintest"})
+    public void pageIsLoaded() throws Exception {
+        logger.info("Test class = " + this.getClass().getSimpleName());
+        logger.info("Test method = " + Thread.currentThread().getStackTrace()[1].getMethodName());
+        driver = invokeBrowser(Config.USER_URL);
+        adminPage = salesModuleSubmit(driver);
+
+        //Side bar
+        adminPage.submitSearch("Admin");
+        String sCell = adminPage.readValueFromTable("1", "2", driver);
+        Assert.assertEquals(sCell, "Admin");
+
+        //Connecting MySQL and retriving the data
+        conn = new MySqlConnect().openDB();
+        String query = "SELECT * FROM pet;";
+        try {
+
+            results = MySqlConnect.stmt.executeQuery(query);
+
+            while (results.next()) {
+                //int id = results.getInt("user_id");
+                String name = results.getString("name");
+                String owner = results.getString("owner");
+                String species = results.getString("species");
+                String birth = results.getString("birth");
+                //String death = results.getString("death");
+
+                // Display Values
+                System.out.println("name: " + name);
+                System.out.println("owner: " + owner);
+                System.out.println("species: " + species);
+                System.out.println("date: " + birth);
+
+                // From GUI
+//        		WebElement element = dv.findElement(By.id("uname"));
+//        		String actualUserName = element.getText();
+        		Assert.assertEquals(sCell, name);
+            }
+            results.close();
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        }
+
+
+        //Debug screenshot
+        MySeleniumMethods.takeScreenshot(driver, getClass().getSimpleName().toString());
+
+    }
+
+}
